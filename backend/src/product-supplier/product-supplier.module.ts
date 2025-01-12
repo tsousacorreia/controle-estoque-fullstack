fd57f6990a82f45a779d7bfc1bdab6f5c5c23bcb
tsousacorreia <tsousacorreia@outlook.com>
2025-01-11 23:03:43 -0300

    feat: corrigir erros

--- backend/src/product-supplier/product-supplier.module.ts
+++ backend/src/product-supplier/product-supplier.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductSupplierController } from './product-supplier.controller';
 import { ProductSupplierService } from './product-supplier.service';
+import { Product } from 'src/product/product.entity';
+import { Supplier } from 'src/supplier/supplier.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product, Supplier])],
   controllers: [ProductSupplierController],
   providers: [ProductSupplierService]
 })
